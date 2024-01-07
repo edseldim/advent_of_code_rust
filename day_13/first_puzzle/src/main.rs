9699230a0b62494403839042bcba97e2d2247807
edseldim <edselmanuel@hotmail.com>
2024-01-07 16:56:35 -0500

    unlocked constraints for row and column match

--- day_13/first_puzzle/src/main.rs
+++ day_13/first_puzzle/src/main.rs
@@ -21,10 +21,15 @@ fn main() {
     for (pattern_num, pattern_vec) in reflection_patterns.iter().enumerate(){
         for (i, reference_pattern) in pattern_vec.iter().enumerate(){
             for (j, checked_pattern) in pattern_vec.iter().enumerate(){ 
-                if reference_pattern == checked_pattern && i != j && i+1 == j{
+                if reference_pattern == checked_pattern && i != j //&& i+1 == j
+                {
                     // horizontal_matches.push(vec![]);
                     // let last_len = horizontal_matches.len() - 1;
-                    horizontal_matches.push((pattern_num as u64, (i+1) as u64, (j+1) as u64, (pattern_vec.len() as f64 / 2.0).ceil() as u64));
+                    let entry_to_be_pushed = (pattern_num as u64, (j+1) as u64, (i+1) as u64, (pattern_vec.len() as f64 / 2.0).ceil() as u64);
+                    if !horizontal_matches.contains(&entry_to_be_pushed){
+                        horizontal_matches.push((pattern_num as u64, (i+1) as u64, (j+1) as u64, (pattern_vec.len() as f64 / 2.0).ceil() as u64));
+                    }
+                    break;
                 }
             }
         }
@@ -36,37 +41,39 @@ fn main() {
     // vertical match (column match)
     let mut vertical_matches: Vec<(u64, u64, u64, u64)> = vec![];
     for (pattern_num, pattern_vec) in reflection_patterns.iter().enumerate(){
-        let mut current_col = 0;
-        while current_col < (pattern_vec[0].len() - 1){
-            let mut correct_pos = 0;
-            // check every row
-            for (i, reference_pattern) in pattern_vec.iter().enumerate(){
-                // check every column
-                let row_chars = reference_pattern.chars().collect::<Vec<char>>();
-                if row_chars[current_col] == row_chars[current_col+1]{
-                    correct_pos += 1;
+        // start comparison shift (second column to be compared)
+        let mut current_shift = 1;
+        while current_shift <= (pattern_vec[0].len() - 1){
+            // start current column (first column to compare the others to)
+            let mut current_col = 0;
+            while current_col < (pattern_vec[0].len() - 1){ // as long as the first column has another to compare to
+                let mut correct_pos = 0;
+                // check every row
+                for (i, reference_pattern) in pattern_vec.iter().enumerate(){
+                    // check every column
+                    let row_chars = reference_pattern.chars().collect::<Vec<char>>();
+                    if row_chars[current_col] == row_chars[current_shift]{
+                        correct_pos += 1;
+                    }
+                }
+                if correct_pos == pattern_vec.len(){
+                    // vertical_matches.push(vec![]);
+                    // let last_len = vertical_matches.len() - 1;
+                    vertical_matches.push((pattern_num as u64, (current_col+1) as u64, (current_shift+1) as u64, (pattern_vec[0].len() as f64 / 2.0).ceil() as u64));
+                }
+                current_col += 1;
+                if current_col == current_shift{
+                    break;
                 }
             }
-            if correct_pos == pattern_vec.len(){
-                // vertical_matches.push(vec![]);
-                // let last_len = vertical_matches.len() - 1;
-                vertical_matches.push((pattern_num as u64, (current_col+1) as u64, (current_col+2) as u64, (pattern_vec[0].len() as f64 / 2.0).ceil() as u64));
-            }
-            current_col += 1;
+
+            current_shift += 1;
         }
+        
     }
 
     println!("vertical_matches: {:?}", vertical_matches);
 
-    let mut horizontal_matches_filtered = horizontal_matches.iter().filter(|x| (x.1 as u64) == x.3).collect::<Vec<_>>();
-    let mut vertical_matches_filtered = vertical_matches.iter().filter(|x| (x.1 as u64) == x.3).collect::<Vec<_>>();
-
-    println!("horizontal_matches: {:?}", horizontal_matches_filtered);
-    println!("vertical_matches: {:?}", vertical_matches_filtered);
-
-    let horizontal_sum: u64 = horizontal_matches_filtered.iter().map(|x| x.1*100).collect::<Vec<u64>>().iter().sum();
-    let vertical_sum: u64 = vertical_matches_filtered.iter().map(|x| x.1).collect::<Vec<u64>>().iter().sum();
-
-    println!("the answer is: {}", horizontal_sum+vertical_sum)
+    
 }
  // BRB
