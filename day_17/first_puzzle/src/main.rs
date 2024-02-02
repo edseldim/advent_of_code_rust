59169892b0e5f7d2115e8ec413418dad67d2e75a
edseldim <edselmanuel@hotmail.com>
2024-02-02 00:29:58 -0500

    add heat path for solution viz

--- day_17/first_puzzle/src/main.rs
+++ day_17/first_puzzle/src/main.rs
@@ -1,7 +1,39 @@
 use std::fs;
 use std::collections::HashMap;
 
+fn direction_to_arrow(direction: String) -> String{
+    if direction == "left"{
+        return "<".to_string()
+    } else if direction == "up"{
+        return "^".to_string()
+    } else if direction == "right"{
+        return ">".to_string()
+    } else {
+        "v".to_string()
+    }
+}
+
+fn print_heat_path(path: Vec<(usize, usize, String, usize)>, matrix: Vec<Vec<usize>>) -> String{
+    let mut matrix_with_path = matrix.clone()
+                                    .iter()
+                                    .map(|row| row.iter().map(|col| col.to_string()).collect::<Vec<String>>())
+                                    .collect::<Vec<Vec<String>>>();
+    
+    for node in path{
+        matrix_with_path[node.0][node.1] = direction_to_arrow(node.2.clone());
+    }
+    
+    let mut heat_map_2d = String::new();
+    for row in matrix_with_path{
+        let mut row_str = row.join("").clone();
+        row_str.push_str("\n");
+        heat_map_2d += &row_str;
+    }
+    heat_map_2d
+}
+
 fn sort_vector(vector_to_be_sorted: &mut Vec<(usize,usize, String, usize)>){
+    // add change index 3 to heat
     for i in 0..vector_to_be_sorted.len(){
         for j in 0..vector_to_be_sorted.len()-i-1{
             if vector_to_be_sorted[j].3 < vector_to_be_sorted[j+1].3{
@@ -11,6 +43,7 @@ fn sort_vector(vector_to_be_sorted: &mut Vec<(usize,usize, String, usize)>){
             }
         }
     }
+    // add change it back to step count with sorted vector
 }
 
 fn get_step_counter(current_head_counter: usize, 
@@ -33,6 +66,7 @@ fn get_neighbours(x: isize, y:isize,
     let right = (0+x,1+y,"right");
     let bottom = (1+x,0+y,"bottom");
     let neighbours_raw = vec![left, up, right, bottom];
+    // return neighbours with correct step count relative to current state
     neighbours_raw.iter()
                 .filter(|node| (node.0 < matrix_rows as isize && node.0 >= 0) && (node.1 < matrix_cols as isize && node.1 >= 0))
                 .map(|node| (node.0 as usize, node.1 as usize, node.2.to_string(), get_step_counter(current_step_direction, current_direction.clone(), node.2.to_string().clone())))
@@ -55,6 +89,7 @@ fn get_opposite_direction(direction_string: &str) -> String{
 fn pop_queue_node(current_queue: &mut Vec<(usize, usize, String, usize)>,
                 current_node_head: &(usize, usize, String, usize)) -> (usize, usize, String, usize){
     let mut copy_current_node_head = current_node_head.clone();
+    // find first node that meets requirements
     let available_next_node_pos = current_queue.iter().enumerate()
                                     .filter(|&(i, node)| (node.2 == copy_current_node_head.2 
                                             || (node.2 != copy_current_node_head.2))
@@ -66,12 +101,6 @@ fn pop_queue_node(current_queue: &mut Vec<(usize, usize, String, usize)>,
     copy_current_node_head.1 = available_next_node.1; // y
     copy_current_node_head.2 = available_next_node.2.clone(); // direction
     copy_current_node_head.3 = available_next_node.3; // direction count
-    // if available_next_node.2 == current_node_head.2{
-    //     copy_current_node_head.3 += 1; // direction count
-    // } else{
-    //     copy_current_node_head.3 = 0;
-    // }
-    // copy_current_node_head.4 += available_next_node.3; // heat count
     copy_current_node_head
 }
 
@@ -93,10 +122,6 @@ fn main() {
                                 .collect::<Vec<Vec<usize>>>();
     // get starting nodes
     let target_node = heat_matrix[heat_matrix.len()-1][heat_matrix[heat_matrix.len()-1].len()-1].clone();
-    let start_node = heat_matrix[0][0].clone();
-    // println!("{:?}", heat_matrix);
-    // println!("starting at {}", start_node);
-    // println!("ending at {}", target_node);
     let mut current_node_head = (0, 0, "right".to_string(), 0);
     dist_map.insert(current_node_head.clone(),0);
     dist_node_map.insert(current_node_head.clone(),current_node_head.clone());
@@ -112,7 +137,7 @@ fn main() {
             if !visited_nodes.iter().any(|node| *node == child_child_node){
                 current_queue.push(child_child_node.clone());
                 visited_nodes.push(child_child_node.clone());
-                
+                // update distance and backtrack map
                 if dist_map.contains_key(&child_child_node){
                     if *dist_map.get(&child_child_node).unwrap() > heat_matrix[child_child_node.0][child_child_node.1] as i32{
                         dist_map.remove(&child_child_node.clone());
@@ -126,18 +151,13 @@ fn main() {
                 }
             }
         }
+        // after all nodes in queue have been visited, stop
         if current_queue.len() == 0{
             break;
         }
         // sort_vector(&mut current_queue);
-        // current_node_head = choose_queue_node(&current_queue, &current_node_head);
-        // visited_nodes.push((current_node_head.0, current_node_head.1, current_node_head.2.clone(), heat_matrix[current_node_head.0][current_node_head.1]));
         current_node_head = pop_queue_node(&mut current_queue,
                                             &current_node_head);
-        println!("new head: {:?}", current_node_head);
-        // println!("visited nodes: {:?}", visited_nodes);
-        println!("queue nodes: {:?}", current_queue);
-        // println!("node dist: {:?}", dist_map);
         // end condition
         // if (current_node_head.0, current_node_head.1) == (heat_matrix.len()-1, heat_matrix[heat_matrix.len()-1].len()-1){
         //     break;
@@ -145,29 +165,31 @@ fn main() {
         
     }
     println!("end nodes: {:?}",dist_node_map);
-    for current_node_head in vec![(12, 12, "bottom".to_string(), 0),
-                                (12, 12, "bottom".to_string(), 1),
-                                (12, 12, "bottom".to_string(), 2),
-                                (12, 12, "right".to_string(), 0),
-                                (12, 12, "right".to_string(), 1),
-                                (12, 12, "right".to_string(), 2),
+    // visit the 6 different ways the target node can be visited
+    for current_node_head in vec![(heat_matrix.len()-1, heat_matrix[heat_matrix.len()-1].len()-1, "bottom".to_string(), 0),
+                                (heat_matrix.len()-1, heat_matrix[heat_matrix.len()-1].len()-1, "bottom".to_string(), 1),
+                                (heat_matrix.len()-1, heat_matrix[heat_matrix.len()-1].len()-1, "bottom".to_string(), 2),
+                                (heat_matrix.len()-1, heat_matrix[heat_matrix.len()-1].len()-1, "right".to_string(), 0),
+                                (heat_matrix.len()-1, heat_matrix[heat_matrix.len()-1].len()-1, "right".to_string(), 1),
+                                (heat_matrix.len()-1, heat_matrix[heat_matrix.len()-1].len()-1, "right".to_string(), 2),
     ]{
-        let mut last_node_backtrack = dist_node_map.get(&current_node_head).unwrap().clone();
-        let mut shortest_path_heat = heat_matrix[last_node_backtrack.0][last_node_backtrack.1];
-        loop{
-            if (last_node_backtrack.0, last_node_backtrack.1) == (0,0){
-                break;
+        if dist_node_map.contains_key(&current_node_head){
+            let mut last_node_backtrack = dist_node_map.get(&current_node_head).unwrap().clone();
+            let mut shortest_path_heat = heat_matrix[last_node_backtrack.0][last_node_backtrack.1];
+            let mut nodes_in_path: Vec<(usize, usize, String, usize)> = vec![];
+            nodes_in_path.push(last_node_backtrack.clone());
+            loop{
+                if (last_node_backtrack.0, last_node_backtrack.1) == (0,0){
+                    break;
+                }
+                last_node_backtrack = dist_node_map.get(&last_node_backtrack).unwrap().clone();
+                nodes_in_path.push(last_node_backtrack.clone());
+                shortest_path_heat += heat_matrix[last_node_backtrack.0][last_node_backtrack.1];
+                // println!("{:?}", last_node_backtrack);
             }
-            last_node_backtrack = dist_node_map.get(&last_node_backtrack).unwrap().clone();
-            shortest_path_heat += heat_matrix[last_node_backtrack.0][last_node_backtrack.1];
-            // println!("{:?}", last_node_backtrack);
+            println!("total heat sum {}", shortest_path_heat);
+            println!("{}", print_heat_path(nodes_in_path, heat_matrix.clone()))
         }
-        println!("total heat sum {}", shortest_path_heat);
     }
     
 }
-
-/*
-Add a way to sort queue by matrix heat while keeping the position at index 3 as the direction count rather than
-the neighbours have it for heap and the node head for direction count
-*/
