1230eb80ea6570e6b70c1de93a2e5a9205cc6fc1
edseldim <edselmanuel@hotmail.com>
2024-02-10 02:27:15 -0500

    day 22 first puzzle solved!

--- day_22/first_puzzle/src/main.rs
+++ day_22/first_puzzle/src/main.rs
@@ -2,44 +2,48 @@ use std::fs;
 use std::cmp;
 use std::collections::{HashMap, HashSet};
 
-fn overlaps(a: &Vec<usize>, b: &Vec<usize>) -> bool{
+fn overlaps(a: &Vec<isize>, b: &Vec<isize>) -> bool{
     //return max(a[0], b[0]) <= min(a[3], b[3]) and max(a[1], b[1]) <= min(a[4], b[4])
-    return cmp::max(a[0], b[0]) <= cmp::min(a[0], b[0]) && cmp::max(a[1], b[1]) <= cmp::min(a[4], b[4])
+    return cmp::max(a[0], b[0]) <= cmp::min(a[3], b[3]) && cmp::max(a[1], b[1]) <= cmp::min(a[4], b[4])
 }
 
 fn main() {
     let file_path = "src/puzzle_data.txt";
-    let mut bricks: Vec<Vec<usize>> = vec![];
+    let mut bricks: Vec<Vec<isize>> = vec![];
     for line in fs::read_to_string(file_path).unwrap().lines(){
         let line_numbers = line.replace("~",",")
                         .split(",")
-                        .map(|number| number.parse().unwrap())
-                        .collect::<Vec<usize>>();
+                        .map(|number| number.parse::<isize>().unwrap())
+                        .collect::<Vec<isize>>();
         bricks.push(line_numbers.clone());
     }
 
-    bricks.sort_by(|a, b| b[2].cmp(&a[2]));
+    bricks.sort_by(|a, b| a[2].cmp(&b[2]));
     println!("{:?}", bricks);
 
     let mut bricks_mut = bricks.clone();
-    for (idx, brick) in bricks_mut.iter_mut().enumerate(){
+    let mut idx = 0;
+    // for (idx, brick) in bricks_mut.iter_mut().enumerate(){
+    while idx < bricks_mut.len(){
         let mut max_z = 1;
-        for check in &bricks[..idx]{
+        let mut brick = &bricks_mut[idx];
+        for check in &bricks_mut[..idx]{
             if overlaps(brick, check){
                 max_z = cmp::max(max_z, check[5] + 1);
             }
         }
-
-        brick[5] -= brick[2] - max_z;
-        brick[2] = max_z;
+        
+        bricks_mut[idx][5] -= bricks_mut[idx][2] - max_z;
+        bricks_mut[idx][2] = max_z;
+        idx += 1;
     }
 
-    bricks_mut.sort_by(|a, b| b[2].cmp(&a[2]));
-
+    bricks_mut.sort_by(|a, b| a[2].cmp(&b[2]));
+    println!("{:?}", bricks_mut);
     let mut k_supports_v: HashMap<usize,HashSet<usize>> = HashMap::new();
     let mut v_supports_k: HashMap<usize,HashSet<usize>> = HashMap::new();
 
-    for i in 0..bricks.len(){
+    for i in 0..bricks_mut.len(){
         k_supports_v.entry(i).or_insert(HashSet::new());
         v_supports_k.entry(i).or_insert(HashSet::new());
     }
@@ -53,9 +57,16 @@ fn main() {
         }
     }
 
+    println!("k-v {:?}", k_supports_v);
+    println!("v-k {:?}", v_supports_k);
+
     let mut total = 0;
 
-    for i in 0..bricks.len(){
-        
+    for i in 0..bricks_mut.len(){
+        if k_supports_v.get(&i).unwrap().iter().all(|j| v_supports_k.get(&j).unwrap().len() >= 2){
+            total += 1;
+        }
     }
+
+    println!("{}", total);
 }
